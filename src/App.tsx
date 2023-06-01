0cd10b1fedfa9d6b405a3dcb8e9ee7c8f9cb6c70
HitoTei <s2313541@u.tsukuba.ac.jp>
2023-06-01 21:56:36 +0900

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    const [taskList, setTaskList] = useState<Task[]>([])
+    return (
+        <div className="App">
+            <button onClick={() => {
+                    setTaskList([...taskList, {name: "Task" + taskList.length, checked: false}])
+            }}>
+                Add task: {taskList.length}
+            </button>
+            <TaskList
+                taskList={taskList}
+                onCheckedChange={(index: number) => {
+                    const newTaskList = taskList.map((task, taskIndex) => {
+                        if (index === taskIndex){
+                            task.checked = !task.checked
+                        }
+                        return task
+                    })
+                    setTaskList(newTaskList)
+                }}
+            />
+        </div>
+    );
+}
+
+interface Task {
+    name: string,
+    checked: boolean
+}
+
+type TaskListProps = {
+    taskList: Task[],
+    onCheckedChange: (index: number) => void
+}
+
+function TaskList(
+    props: TaskListProps
+) {
+
+    return <div className="TaskList">
+        {props.taskList.map((task, index) =>
+            <TaskTile
+                taskName={task.name}
+                checked={task.checked}
+                onChange={() => props.onCheckedChange(index)}
+            />
+        )}
     </div>
-  );
+
+}
+
+function TaskTile({
+                      taskName = "",
+                      checked = false,
+                      onChange = () => {
+                      }
+                  }) {
+    return <div className="TaskTile">
+        <input
+            className="TaskTileCheckBox"
+            type="checkbox"
+            checked={checked}
+            onChange={onChange}
+        />
+        {taskName}
+    </div>
 }
 
 export default App;
